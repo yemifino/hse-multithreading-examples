f56d26ff6c88eb490abfd20722f514cafc6877ae
Степа <yem@MacBook-Pro-Stepa.local>
2026-03-23 22:44:14 +0300

    dz2: reduce wakeups in unbuffered channel

--- tasks/synchronization_primitives/unbuffered_channel/unbuffered_channel.h
+++ tasks/synchronization_primitives/unbuffered_channel/unbuffered_channel.h
@@ -13,16 +13,16 @@ public:
     void Send(const T& value) {
         std::unique_lock lock(mutex_);
 
-        send_cv_.wait(lock, [this]() { return closed_ || !slot_.has_value(); });
+        send_ready_cv_.wait(lock, [this]() { return closed_ || !slot_.has_value(); });
         if (closed_) {
             throw std::runtime_error("Channel is closed");
         }
 
         const std::size_t my_seq = ++send_seq_;
         slot_.emplace(value);
-        recv_cv_.notify_one();
+        recv_ready_cv_.notify_one();
 
-        send_cv_.wait(lock, [this, my_seq]() { return closed_ || recv_seq_ >= my_seq; });
+        send_done_cv_.wait(lock, [this, my_seq]() { return closed_ || recv_seq_ >= my_seq; });
         if (closed_ && recv_seq_ < my_seq) {
             throw std::runtime_error("Channel is closed");
         }
@@ -30,7 +30,7 @@ public:
 
     std::optional<T> Recv() {
         std::unique_lock lock(mutex_);
-        recv_cv_.wait(lock, [this]() { return closed_ || slot_.has_value(); });
+        recv_ready_cv_.wait(lock, [this]() { return closed_ || slot_.has_value(); });
 
         if (!slot_.has_value()) {
             return std::nullopt;
@@ -39,8 +39,8 @@ public:
         T value = std::move(*slot_);
         slot_.reset();
         recv_seq_ = send_seq_;
-        lock.unlock();
-        send_cv_.notify_all();
+        send_done_cv_.notify_one();
+        send_ready_cv_.notify_one();
         return value;
     }
 
@@ -53,14 +53,16 @@ public:
             closed_ = true;
             slot_.reset();
         }
-        send_cv_.notify_all();
-        recv_cv_.notify_all();
+        send_ready_cv_.notify_all();
+        send_done_cv_.notify_all();
+        recv_ready_cv_.notify_all();
     }
 
 private:
     std::mutex mutex_;
-    std::condition_variable send_cv_;
-    std::condition_variable recv_cv_;
+    std::condition_variable send_ready_cv_;
+    std::condition_variable send_done_cv_;
+    std::condition_variable recv_ready_cv_;
     std::optional<T> slot_;
     bool closed_ = false;
     std::size_t send_seq_ = 0;
